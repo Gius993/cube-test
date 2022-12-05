9c629c7eb9ad829848dc8fcb08dcec33ba8c6180
Gius993 <giuseppevidili9@gmail.com>
2022-12-05 16:57:49 +0100

    animation xy

--- js/script.js
+++ js/script.js
@@ -29,7 +29,21 @@ let cx = w / 2;
 let cy = h / 2;
 let cz = 0;
 let size = h / 4;
-
+let vertices = [
+	new POINT3D(cx - size, cy - size, cz - size),
+	new POINT3D(cx + size, cy - size, cz - size),
+	new POINT3D(cx + size, cy + size, cz - size),
+	new POINT3D(cx - size, cy + size, cz - size),
+	new POINT3D(cx - size, cy - size, cz + size),
+	new POINT3D(cx + size, cy - size, cz + size),
+	new POINT3D(cx + size, cy + size, cz + size),
+	new POINT3D(cx - size, cy + size, cz + size)
+];
+let edges = [
+	[0, 1], [1, 2], [2, 3], [3, 0], // back face
+	[4, 5], [5, 6], [6, 7], [7, 4], // front face
+	[0, 4], [1, 5], [2, 6], [3, 7], // connecting sides
+];
 //set up the animation loop
 let timeDelta, timeLast = 0;
 requestAnimationFrame(loop);
@@ -42,6 +56,25 @@ function loop(timeNow){
 
 	//background
 	ctx.fillRect(0, 0, w, h);
+
+	//rotate the cube along the z axis
+	let angle = timeDelta * 0.001 * SPED_Z * Math.PI * 2;
+	for(let v of vertices){
+		let dx = v.x - cx;
+		let dy = v.y - cy;
+		let x = dx * Math.cos(angle) - dy * Math.sin(angle);
+		let y= dx * Math.sin(angle) + dy * Math.cos(angle);
+		v.x = x + cx;
+		v.y = y + cy;
+	}
+
+	//draw each edge
+	for(let edge of edges){
+		ctx.beginPath();
+		ctx.moveTo(vertices[edge[0]].x, vertices[edge[0]].y);
+		ctx.lineTo(vertices[edge[1]].x, vertices[edge[1]].y);
+		ctx.stroke();
+	}
 
 	//call the next frame
 	requestAnimationFrame(loop);
